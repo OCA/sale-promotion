2f65ff9a07fb776a579fdbb360e63a797557cba3
david <david.vidal@tecnativa.com>
2021-07-27 13:50:23 +0200

    [ADD] sale_coupon_incompatibility: New module
    TT30846

---
+++ sale_loyalty_incompatibility/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Coupon incompatibilities",
+    "summary": "Allows to set incompatibility rules between promotions",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["sale_coupon"],
+    "data": ["views/sale_coupon_program_views.xml"],
+}
---
+++ sale_loyalty_incompatibility/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to set incompatibilities between promotions, so when a promotion A
+is not compatible with promotion B, they can't be applied together in a sales order.
