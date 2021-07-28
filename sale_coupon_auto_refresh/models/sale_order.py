03a6ffb750f2340dda2383c29dd252bc3c2bcd5d
SilvioC2C <silvio.gregorini@camptocamp.com>
2021-07-28 18:03:53 +0200

    [13.0][UPD] sale_coupon_auto_refresh: performance optimization, lesser refactoring for better inheritability

--- sale_coupon_auto_refresh/models/sale_order.py
+++ sale_coupon_auto_refresh/models/sale_order.py
@@ -1,5 +1,7 @@
 # Copyright 2021 Tecnativa - David Vidal
+# Copyright 2021 Camptocamp - Silvio Gregorini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models
 
 
@@ -9,22 +11,55 @@ class SaleOrder(models.Model):
     # Used in UI to hide the manual button
     auto_refresh_coupon = fields.Boolean(related="company_id.auto_refresh_coupon",)
 
-    def _auto_refresh_coupons(self):
-        self.filtered(
-            lambda x: x.state in ("draft", "sent") and x.auto_refresh_coupon
-        ).recompute_coupon_lines()
-
     @api.model_create_multi
     def create(self, vals_list):
-        """Create or refresh coupon lines on create"""
         orders = super().create(vals_list)
-        orders.with_context(skip_auto_refresh_coupons=True)._auto_refresh_coupons()
+        orders._auto_refresh_coupons()
         return orders
 
     def write(self, vals):
-        """Create or refresh coupon lines after saving"""
         res = super().write(vals)
-        if self.env.context.get("skip_auto_refresh_coupons"):
-            return res
-        self.with_context(skip_auto_refresh_coupons=True)._auto_refresh_coupons()
+        self._auto_refresh_coupons()
+        return res
+
+    def _auto_refresh_coupons(self):
+        if not self.env.context.get("skip_auto_refresh_coupons"):
+            orders = self.filtered(type(self)._allow_recompute_coupon_lines)
+            if orders:
+                orders = orders.with_context(skip_auto_refresh_coupons=True)
+                orders.recompute_coupon_lines()
+
+    def _allow_recompute_coupon_lines(self):
+        """Returns whether reward lines in order ``self`` can be recomputed
+        automatically.
+
+        Hook method, to be overridden for custom behaviours.
+
+        :return: True if the current SO allows automatic recomputation for
+        reward lines, False otherwise.
+        """
+        self.ensure_one()
+        return self.auto_refresh_coupon and self.state in ("draft", "sent")
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super().create(vals_list)
+        lines.mapped("order_id")._auto_refresh_coupons()
+        return lines
+
+    def write(self, vals):
+        orders = self.mapped("order_id")
+        res = super().write(vals)
+        orders |= self.mapped("order_id")
+        orders._auto_refresh_coupons()
+        return res
+
+    def unlink(self):
+        orders = self.mapped("order_id")
+        res = super().unlink()
+        orders._auto_refresh_coupons()
         return res
