fac42286998c1df22ff5973a7771f2233250278a
SilvioC2C <silvio.gregorini@camptocamp.com>
2021-07-28 18:03:53 +0200

    [13.0][UPD] sale_coupon_auto_refresh: performance optimization, lesser refactoring for better inheritability

--- sale_coupon_auto_refresh/__manifest__.py
+++ sale_coupon_auto_refresh/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Tecnativa - David Vidal
+# Copyright 2021 Camptocamp - Silvio Gregorini
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Auto Refresh Coupons",
@@ -7,8 +8,8 @@
     "development_status": "Production/Stable",
     "category": "Website",
     "website": "https://github.com/OCA/sale-promotion",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
-    "maintainers": ["chienandalu"],
+    "author": "Tecnativa, Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu", "SilvioC2C"],
     "license": "AGPL-3",
     "depends": ["sale_coupon"],
     "data": ["views/res_config_settings_views.xml"],
