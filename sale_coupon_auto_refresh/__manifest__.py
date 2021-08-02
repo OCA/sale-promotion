ad50ef60a4ea7723fc70efcb734efc72b205160d
SilvioC2C <silvio.gregorini@camptocamp.com>
2021-08-02 17:11:05 +0200

    [MIG] sale_coupon_auto_refresh: migration to 14.0

--- sale_coupon_auto_refresh/__manifest__.py
+++ sale_coupon_auto_refresh/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Auto Refresh Coupons",
     "summary": "Allows to auto-apply the coupons with no user intervention",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Website",
     "website": "https://github.com/OCA/sale-promotion",
