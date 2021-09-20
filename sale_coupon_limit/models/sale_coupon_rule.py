d66d0da3212f1208fbff02267b1e9b993fe4f902
david <david.vidal@tecnativa.com>
2021-09-20 17:14:43 +0200

    [IMP] sale_coupon_limit: usage info
    We add some compute fields to check the current promotion usage by the
    limited users. That allows us to refactor the check method a little bit
    as well unifying the logic.
    
    TT30847

--- sale_coupon_limit/models/sale_coupon_rule.py
+++ sale_coupon_limit/models/sale_coupon_rule.py
@@ -28,14 +28,24 @@ class SaleCouponRule(models.Model):
         string="Salesmen maximum promotions",
         compute="_compute_rule_salesmen_limit_count",
     )
+    rule_salesmen_limit_used_count = fields.Integer(
+        string="Salesmen promotions used", compute="_compute_rule_salesmen_limit_count",
+    )
 
-    @api.depends("rule_salesmen_limit_ids.rule_max_salesman_application")
+    @api.depends(
+        "rule_salesmen_limit_ids.rule_max_salesman_application",
+        "rule_salesmen_limit_ids.rule_times_used",
+    )
     def _compute_rule_salesmen_limit_count(self):
+        """This count is merely informative"""
+        self.rule_salesmen_limit_count = 0
+        self.rule_salesmen_limit_used_count = 0
         for rule in self:
             rule.rule_salesmen_limit_count = sum(
-                rule.rule_salesmen_limit_ids.mapped(
-                    "rule_salesmen_limit_ids.rule_max_salesman_application"
-                )
+                rule.rule_salesmen_limit_ids.mapped("rule_max_salesman_application")
+            )
+            rule.rule_salesmen_limit_used_count = sum(
+                rule.rule_salesmen_limit_ids.mapped("rule_times_used")
             )
 
 
@@ -57,6 +67,7 @@ class SaleCouponRuleSalesmenLimit(models.Model):
         default=0,
         help="Maximum times a salesman can apply a program. 0 for no limit.",
     )
+    rule_times_used = fields.Integer(string="Uses", compute="_compute_rule_times_used",)
 
     _sql_constraints = [
         (
@@ -65,3 +76,50 @@ class SaleCouponRuleSalesmenLimit(models.Model):
             "This salesman limit is already configured",
         ),
     ]
+
+    @api.depends("rule_user_id", "rule_max_salesman_application")
+    def _compute_rule_times_used(self):
+        """This count is also used in the check methods to avoid applying the rule
+        above the salesmen limits."""
+        self.rule_times_used = 0
+        programs = self.env["sale.coupon.program"].search_read(
+            [("rule_id", "in", self.mapped("rule_id").ids)],
+            ["id", "rule_id", "program_type", "coupon_ids", "rule_salesmen_limit_ids"],
+        )
+        coupon_programs = [
+            p
+            for p in programs
+            if p["program_type"]
+            and p["program_type"] == "coupon_program"
+            or p["coupon_ids"]
+        ]
+        for program in programs:
+            salesmen_limits = self.filtered(
+                lambda x: x._origin.id in program["rule_salesmen_limit_ids"]
+            )
+            for salesman_limit in salesmen_limits:
+                if program in coupon_programs and not program["coupon_ids"]:
+                    continue
+                elif program in coupon_programs:
+                    salesman_limit.rule_times_used = self.env[
+                        "sale.coupon"
+                    ].search_count(
+                        [
+                            ("program_id", "=", program["id"]),
+                            ("state", "=", "used"),
+                            (
+                                "sales_order_id.user_id",
+                                "=",
+                                salesman_limit.rule_user_id.id,
+                            ),
+                        ]
+                    )
+                    continue
+                salesman_limit.rule_times_used = self.env["sale.order"].search_count(
+                    [
+                        "|",
+                        ("no_code_promo_program_ids", "in", [program["id"]]),
+                        ("code_promo_program_id", "=", [program["id"]]),
+                        ("user_id", "=", salesman_limit.rule_user_id.id),
+                    ]
+                )
