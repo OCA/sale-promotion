f9773331ba2d5cf56e1c2e7ba41e46e3dcf5da27
david <david.vidal@tecnativa.com>
2021-09-28 14:42:05 +0200

    [IMP] website_sale_coupon_restrict: black, isort, prettier

--- website_sale_coupon_restrict/__manifest__.py
+++ website_sale_coupon_restrict/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Website",
-    "website": "https://github.com/OCA/e-commerce.git",
+    "website": "https://github.com/OCA/sale-promotion",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
