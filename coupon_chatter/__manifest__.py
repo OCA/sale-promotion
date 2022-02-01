f09a47202babef550373ee51f0b948e50dfaa339
david <david.vidal@tecnativa.com>
2022-02-01 11:10:36 +0100

    [ADD] sale_coupon_chatter: New module
    TT34237

---
+++ coupon_chatter/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Coupon Chatter",
+    "summary": "Register messages and activities on the sale coupon records",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["sale_coupon"],
+    "data": ["views/sale_coupon_program_views.xml"],
+}
