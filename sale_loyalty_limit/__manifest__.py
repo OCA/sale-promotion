d5db3729991a66b1389355235a1aa76f61712351
david <david.vidal@tecnativa.com>
2022-02-09 16:36:01 +0100

    [FIX] sale_coupon_limit: improve rule times used count
    Sometimes Odoo doesn't remove the rules properly until the promotions
    are refreshed. This could lead to count mismatches. We wan't to ensure
    the count looking at the lines instead.
    
    TT34266

--- sale_loyalty_limit/__manifest__.py
+++ sale_loyalty_limit/__manifest__.py
@@ -3,13 +3,17 @@
 {
     "name": "Sale Coupon Limit",
     "summary": "Restrict number of promotions per customer or salesman",
-    "version": "13.0.1.1.2",
+    "version": "13.0.1.1.3",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-promotion",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
-    "depends": ["sale_coupon", "sale_commercial_partner"],
+    "depends": [
+        "sale_coupon",
+        "sale_commercial_partner",
+        "sale_coupon_order_line_link",
+    ],
     "data": ["views/sale_coupon_program_views.xml", "security/ir.model.access.csv"],
 }
