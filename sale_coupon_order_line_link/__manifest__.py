063b4ce96c66f086bea0da9521efb70a2f6ee301
david <david.vidal@tecnativa.com>
2022-03-30 14:08:23 +0200

    [IMP] sale_coupon_order_line_link: view applied promotions
    We can now view which promotions are applied visually.
    
    TT35401

--- sale_coupon_order_line_link/__manifest__.py
+++ sale_coupon_order_line_link/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Link coupons to order lines",
     "summary": "Adds a link between coupons and their generated order lines for easing "
     "tracking",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.2.0",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-promotion",
@@ -12,5 +12,5 @@
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
     "depends": ["sale_coupon"],
-    "data": ["reports/sale_report_views.xml"],
+    "data": ["reports/sale_report_views.xml", "views/sale_order_views.xml"],
 }
