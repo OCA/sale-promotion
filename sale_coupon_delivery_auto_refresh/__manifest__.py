5cc75e8cf854998d64594d41436320939885fe10
diga <gabbasov@itlibertas.com>
2022-05-03 02:18:32 +0500

    [14.0][ADD] sale_coupon_delivery_auto_refresh

---
+++ sale_coupon_delivery_auto_refresh/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Auto Refresh Coupons with Delivery line",
+    "version": "14.0.1.0.0",
+    "summary": "Allows to auto-apply the coupons after adding delivery line",
+    "author": "Ooops404, Cetmix, , Odoo Community Association (OCA)",
+    "contributors": "Ooops404, Cetmix",
+    "license": "LGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_coupon_auto_refresh", "delivery_auto_refresh"],
+    "external_dependencies": {},
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
