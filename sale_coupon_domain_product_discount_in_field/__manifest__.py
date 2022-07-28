fc6e66b9e77d69f756b11053835f1f42a9bbc5a3
diga <gabbasov@itlibertas.com>
2022-07-29 00:38:01 +0500

    [ADD] sale_coupon_domain_product_discount_in_field: Compatibility 2 modules

---
+++ sale_coupon_domain_product_discount_in_field/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Ooops404
+# Copyright 2022 Dinar Gabbasov
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Coupon Promotion Product Domain Discount in Field",
+    "version": "14.0.1.0.0",
+    "summary": "Apply discount only to the domain matching products to discount field",
+    "author": "Ooops, Cetmix, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": [
+        "sale_promotion_discount_in_field",
+        "sale_coupon_domain_product_discount",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+    "maintainers": ["isserver1", "GabbasovDinar"],
+}
---
+++ sale_coupon_domain_product_discount_in_field/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Coupon Promotion Product Domain Discount in Field
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Bridge module between sale_promotion_discount_in_field and sale_coupon_domain_product_discount
