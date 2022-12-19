3d6eec24dea29f573585e356addca0777a672565
Stefan <stefan.ungureanu@tecnativa.com>
2022-12-19 12:32:01 +0100

    [IMP] website_sale_coupon_page: black, isort, prettier

--- website_sale_loyalty_page/static/src/js/website_sale_coupon_page_admin.js
+++ website_sale_loyalty_page/static/src/js/website_sale_coupon_page_admin.js
@@ -1,7 +1,7 @@
 /* Copyright 2020 Jairo Llopis - Tecnativa
  * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-odoo.define("website_sale_coupon_page.tour_admin", function(require) {
+odoo.define("website_sale_coupon_page.tour_admin", function (require) {
     "use strict";
 
     const tour = require("web_tour.tour");
