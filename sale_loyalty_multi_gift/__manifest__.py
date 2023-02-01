f6c8e5b7b03d4c8f3caa25ca5362b2a58ad717d8
David <david.vidal@tecnativa.com>
2023-02-01 13:59:35 +0100

    [IMP] coupon_multi_gift, sale_coupon_multi_gift: split modules

--- sale_loyalty_multi_gift/__manifest__.py
+++ sale_loyalty_multi_gift/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Coupons multi gift",
-    "summary": "Allows to configure multiple gift rewards per promotion",
+    "name": "Coupons multi gift in sales",
+    "summary": "Allows to configure multiple gift rewards per promotion in sales",
     "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Sale",
@@ -10,6 +10,6 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
-    "depends": ["sale_coupon_order_line_link"],
-    "data": ["views/coupon_program_views.xml", "security/ir.model.access.csv"],
+    "depends": ["sale_coupon_order_line_link", "coupon_multi_gift"],
+    "data": ["security/ir.model.access.csv"],
 }
