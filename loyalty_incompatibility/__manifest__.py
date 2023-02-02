e403cc05a9acef683c0aa0967718ddd6dd450645
Stefan <stefan.ungureanu@tecnativa.com>
2023-02-02 11:48:05 +0100

    [IMP] coupon_incompatibility, sale_coupon_incompatibility
    Allow coupon_incompatibility settings to be used by other modules.

---
+++ loyalty_incompatibility/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Tecnativa - David Vidal
+# Copyright 2023 Tecnativa - Stefan Ungureanu
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Coupon incompatibilities",
+    "summary": "Allows to set incompatibility rules between promotions",
+    "version": "15.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["coupon"],
+    "data": ["views/sale_coupon_program_views.xml"],
+}
