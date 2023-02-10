07e335b978861e5418467f4c7b1fde82b528abd2
Carlos Roca <carlos.roca@tecnativa.com>
2023-02-10 12:28:35 +0100

    [ADD] sale_coupon_portal: New module to see your coupons on portal view

---
+++ sale_coupon_portal/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Coupon Portal",
+    "summary": "Add possibility to see your coupons on portal",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["CarlosRoca13"],
+    "license": "AGPL-3",
+    "depends": ["sale_coupon"],
+    "data": ["templates/assets.xml", "templates/coupon_portal_templates.xml"],
+}
