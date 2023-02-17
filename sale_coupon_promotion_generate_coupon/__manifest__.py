6e0b704afc91cc27df8e57fb4f0d1181f6547656
David <david.vidal@tecnativa.com>
2023-02-17 13:11:46 +0100

    [ADD] sale_coupon_promotion_generate_coupon: New module
    TT41521

---
+++ sale_coupon_promotion_generate_coupon/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Generate coupons in another coupon program",
+    "summary": "Allows to generate pending coupons in a coupon program",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_coupon"],
+    "data": ["views/coupon_program_views.xml"],
+}
