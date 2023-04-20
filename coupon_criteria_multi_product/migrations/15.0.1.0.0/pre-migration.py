d3c200e148c8eeedb5e72843b81b81422910abe4
David <david.vidal@tecnativa.com>
2023-04-20 11:05:06 +0200

    [OU-ADD] coupon_criteria_multi_product: migration script
    TT39032

--- coupon_criteria_multi_product/migrations/15.0.1.0.0/pre-migration.py
+++ coupon_criteria_multi_product/migrations/15.0.1.0.0/pre-migration.py
@@ -9,7 +9,18 @@ _model_renames = [
 _table_renames = [("sale_coupon_criteria", "coupon_criteria")]
 
 
+def install_new_modules(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE ir_module_module
+        SET state='to install'
+        WHERE name = 'sale_coupon_criteria_multi_product' AND state='uninstalled'""",
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.rename_models(env.cr, _model_renames)
     openupgrade.rename_tables(env.cr, _table_renames)
+    install_new_modules(env)
