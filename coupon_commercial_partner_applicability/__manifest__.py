7213486313cf97c684eac4f08d5bec6dfa11ed19
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-05-30 11:30:30 +0200

    [MIG] sale_coupon_commercial_partner_applicability: Renamed to coupon_commercial_partner_applicability + Migration to 15.0
    TT43554

--- coupon_commercial_partner_applicability/__manifest__.py
+++ coupon_commercial_partner_applicability/__manifest__.py
@@ -3,12 +3,12 @@
 {
     "name": "Coupons on Commercial Entity",
     "summary": "Coupons can be used by all the commercial entity partners",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-promotion",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
-    "depends": ["sale_coupon"],
+    "depends": ["coupon"],
 }
--- coupon_commercial_partner_applicability/models/coupon_coupon.py
+++ coupon_commercial_partner_applicability/models/coupon_coupon.py
@@ -3,18 +3,18 @@
 from odoo import _, models
 
 
-class SaleCoupon(models.Model):
-    _inherit = "sale.coupon"
+class Coupon(models.Model):
+    _inherit = "coupon.coupon"
 
-    def _check_coupon_code(self, order):
+    def _check_coupon_code(self, order_date, partner_id, **kwargs):
         """All the partners in the commercial entity can use their common coupons"""
-        message = super()._check_coupon_code(order)
+        message = super()._check_coupon_code(order_date, partner_id, **kwargs)
         # String comparison isn't a great way to check it, but we don't have a better
         # hook as there's no way to check if the error came before or after that fail
         if message.get("error", "") == _("Invalid partner."):
             if (
                 self.partner_id.commercial_partner_id
-                == order.partner_id.commercial_partner_id
+                == self.env["res.partner"].browse(partner_id).commercial_partner_id
             ):
                 message.pop("error")
         return message
