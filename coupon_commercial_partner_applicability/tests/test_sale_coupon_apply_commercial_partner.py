cc5af53698d2fd316b5f9bfdf43b0cfe8d9330f0
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-05-30 11:30:30 +0200

    [MIG] sale_coupon_commercial_partner_applicability: Renamed to coupon_commercial_partner_applicability + Migration to 15.0
    TT43554

--- coupon_commercial_partner_applicability/tests/test_sale_coupon_apply_commercial_partner.py
+++ coupon_commercial_partner_applicability/tests/test_sale_coupon_apply_commercial_partner.py
@@ -1,9 +1,9 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import Form, SavepointCase
+from odoo.tests import Form, TransactionCase
 
 
-class SaleCouponApplyCommercialPartnerCase(SavepointCase):
+class SaleCouponApplyCommercialPartnerCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -14,12 +14,12 @@ class SaleCouponApplyCommercialPartnerCase(SavepointCase):
         cls.child_2 = cls.child_1.copy({"name": "Odoo Jr."})
         cls.other_partner = cls.commercial_entity.copy({"name": "Mr. OCA"})
         coupon_program_form = Form(
-            cls.env["sale.coupon.program"],
-            view="sale_coupon.sale_coupon_program_view_form",
+            cls.env["coupon.program"],
+            view="coupon.coupon_program_view_coupon_program_form",
         )
         coupon_program_form.name = "Test Apply Commercial Entity"
         cls.code_promotion_program = coupon_program_form.save()
-        cls.env["sale.coupon.generate"].with_context(
+        cls.env["coupon.generate.wizard"].with_context(
             active_id=cls.code_promotion_program.id
         ).create(
             {
@@ -31,21 +31,3 @@ class SaleCouponApplyCommercialPartnerCase(SavepointCase):
         cls.product_1 = cls.env["product.product"].create(
             {"name": "Test 1", "sale_ok": True, "list_price": 50}
         )
-        sale_form = Form(cls.env["sale.order"])
-        sale_form.partner_id = cls.child_2
-        with sale_form.order_line.new() as line_form:
-            line_form.product_id = cls.product_1
-            line_form.product_uom_qty = 2
-        cls.sale = sale_form.save()
-
-
-class TestSaleCouponApplyCommercialPartner(SaleCouponApplyCommercialPartnerCase):
-    def test_sale_coupon_apply_commercial_partner(self):
-        self.assertEqual(self.coupon.state, "new")
-        msg = self.coupon._check_coupon_code(self.sale)
-        # We can use the coupon with other partner of the entity
-        self.assertFalse(msg.get("error"))
-        self.coupon.partner_id = self.other_partner
-        # The expected behavior with other partners
-        msg = self.coupon._check_coupon_code(self.sale)
-        self.assertTrue(bool(msg.get("error")))
