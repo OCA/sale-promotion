e1b5580cec8c72d69ffd10c1b9222f6b2e5115a8
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-09-27 11:23:52 +0200

    [MIG] sale_coupon_partner: Renamed to sale_loyalty_partner + Migration to version 16.0
    TT44351

--- sale_loyalty_partner/reports/sale_report.py
+++ sale_loyalty_partner/reports/sale_report.py
@@ -7,23 +7,22 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    coupon_program_partner_id = fields.Many2one(
+    loyalty_program_partner_id = fields.Many2one(
         comodel_name="res.partner",
-        string="Coupon Program Partner",
+        string="Loyalty Program Partner",
     )
 
-    def _query(self, with_clause="", fields=None, groupby="", from_clause=""):
-        if fields is None:
-            fields = {}
-        select_str = """ ,
-            scp.partner_id as coupon_program_partner_id
-        """
-        from_clause += "left join coupon_program scp on (l.coupon_program_id = scp.id)"
-        fields.update({"coupon_program_partner_id": select_str})
-        groupby += ", scp.partner_id"
-        return super()._query(
-            with_clause=with_clause,
-            fields=fields,
-            groupby=groupby,
-            from_clause=from_clause,
-        )
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res["loyalty_program_partner_id"] = "scp.partner_id"
+        return res
+
+    def _from_sale(self):
+        res = super()._from_sale()
+        res += """ left join loyalty_program scp on (l.loyalty_program_id = scp.id)"""
+        return res
+
+    def _group_by_sale(self):
+        res = super()._group_by_sale()
+        res += """, scp.partner_id"""
+        return res
