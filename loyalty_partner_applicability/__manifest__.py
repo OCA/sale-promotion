50c4e0774d6ad7d5febd62f5ae08106b6ba86576
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-10-09 09:58:01 +0200

    [ADD] loyalty_partner_applicability: New module
    TT45413

---
+++ loyalty_partner_applicability/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Loyalty Partner Applicability",
+    "summary": "Enables the definition of a customer filter for promotion rules that will "
+    "only be applied to customers who meet the specified conditions in the filter.",
+    "version": "16.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["loyalty"],
+    "data": ["views/loyalty_rule_view_form.xml"],
+}
