f2d48921c8ddc07c44af05e5967e131ba23a7417
Luis Ernesto Garcia Medina <ernesto.garcia@tecnativa.com>
2023-10-16 04:20:39 -0600

    [MIG] coupon_criteria_multi_product: Migration to 16.0 and rename to loyalty_criteria_multi_product

--- loyalty_criteria_multi_product/models/loyalty_program.py
+++ loyalty_criteria_multi_product/models/loyalty_program.py
@@ -1,30 +1,30 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
-class CouponProgram(models.Model):
-    _inherit = "coupon.program"
+class LoyaltyProgram(models.Model):
+    _inherit = "loyalty.program"
 
-    coupon_criteria = fields.Selection(
+    loyalty_criteria = fields.Selection(
         selection=[("domain", "Domain"), ("multi_product", "Multi Product")],
-        string="Coupon criteria",
+        string="Loyalty criteria",
         help="- Domain: Standard behavior. The products are evaluated by domain.\n"
         "- Multi product: different rules can be applied to different products "
         "and all of the have to be fulfilled",
         default="domain",
     )
-    coupon_criteria_ids = fields.One2many(
+    loyalty_criteria_ids = fields.One2many(
         string="Multi Product Criterias",
-        comodel_name="coupon.criteria",
+        comodel_name="loyalty.criteria",
         inverse_name="program_id",
     )
 
-    @api.onchange("coupon_criteria")
-    def _onchange_coupon_criteria(self):
-        """Clear domain so we clear some other fields from the view"""
-        if self.coupon_criteria == "multi_product":
-            self.rule_products_domain = False
+    # @api.onchange("loyalty_criteria")
+    # def _onchange_loyalty_criteria(self):
+    #     """Clear domain so we clear some other fields from the view"""
+    #     if self.loyalty_criteria == "multi_product":
+    #         self.rule_products_domain = False
 
     def _get_valid_products_multi_product(self, products, criteria):
         """Return valid products depending on the criteria repeat product setting. Then
