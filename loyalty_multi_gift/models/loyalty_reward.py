0ee1ad2837630cacec8fd64226baff1c9432b308
Luis Ernesto Garcia Medina <ernesto.garcia@tecnativa.com>
2023-10-17 20:46:08 -0600

    [MIG] coupon_multi_gift: Migration to 16.0 and rename to loyalty_multi_gift

--- loyalty_multi_gift/models/loyalty_reward.py
+++ loyalty_multi_gift/models/loyalty_reward.py
@@ -3,36 +3,39 @@
 from odoo import _, api, fields, models
 
 
-class CouponReward(models.Model):
-    _inherit = "coupon.reward"
+class LoyaltyReward(models.Model):
+    _inherit = "loyalty.reward"
 
-    coupon_multi_gift_ids = fields.One2many(
-        comodel_name="coupon.reward.product_line",
+    loyalty_multi_gift_ids = fields.One2many(
+        comodel_name="loyalty.reward.product_line",
         inverse_name="reward_id",
         string="Gift list",
     )
-    reward_type = fields.Selection(selection_add=[("multi_gift", "Multi Gift")])
+    reward_type = fields.Selection(
+        selection_add=[("multi_gift", "Multi Gift")],
+        ondelete={"multi_gift": "set default"},
+    )
 
     def name_get(self):
         """Add complete description for the multi gift reward type."""
         res = super().name_get()
-        for reward in self.filtered(lambda x: x.reward_type == "multi_gift"):
+        for reward in self.filtered(lambda x: x.program_type == "multi_gift"):
             reward_string = _("Free Products - %(name)s") % {
                 "name": ", ".join(
                     f"{reward.reward_product_quantity}x "
                     f"{fields.first(reward.reward_product_ids).name}"
-                    for reward in reward.coupon_multi_gift_ids
+                    for reward in reward.loyalty_multi_gift_ids
                 )
             }
             res.append((reward.id, reward_string))
         return res
 
 
-class CouponGift(models.Model):
-    _name = "coupon.reward.product_line"
-    _description = "Coupon Multi Gift"
+class LoyaltyGift(models.Model):
+    _name = "loyalty.reward.product_line"
+    _description = "Loyalty Multi Gift"
 
-    reward_id = fields.Many2one(comodel_name="coupon.reward")
+    reward_id = fields.Many2one(comodel_name="loyalty.reward")
     reward_product_quantity = fields.Integer(
         string="Quantity",
         help="Reward product quantity",
@@ -52,7 +55,7 @@ class CouponGift(models.Model):
     @api.depends("reward_product_ids")
     def _compute_reward_default_product_id(self):
         """This field acts as a cover for a simple many2one behavior of the module.
-        Another module like `sale_coupon_selection_wizard` can dismiss it in order
+        Another module like `sale_loyalty_selection_wizard` can dismiss it in order
         to allow optional"""
         for line in self:
             line.reward_default_product_id = fields.first(line.reward_product_ids)
