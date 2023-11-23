3cf05179e069ec72fde218ecbc5c7af0efb4b3d8
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-11-23 12:10:55 +0100

    [MIG] sale_coupon_multi_gift>sale_loyalty_multi_gift: Migration to 16.0
    TT44348

--- sale_loyalty_multi_gift/__manifest__.py
+++ sale_loyalty_multi_gift/__manifest__.py
@@ -1,15 +1,18 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Coupons multi gift in sales",
+    "name": "Sale Loyalty Multi Gift",
     "summary": "Allows to configure multiple gift rewards per promotion in sales",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-promotion",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
-    "depends": ["sale_coupon_order_line_link", "coupon_multi_gift"],
-    "data": ["security/ir.model.access.csv"],
+    "depends": ["sale_loyalty_order_line_link", "loyalty_multi_gift"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/sale_loyalty_reward_wizard_views.xml",
+    ],
 }
