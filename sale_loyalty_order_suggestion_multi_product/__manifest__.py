adf396549f9002e2da37933cab1736c98bc5010e
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-01-29 18:59:11 +0100

    [ADD] sale_loyalty_order_suggestion_multi_product: New module
    TT47449

---
+++ sale_loyalty_order_suggestion_multi_product/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Loyalty Order Suggestion Multi Product",
+    "summary": "Suggest promotions with criteria multi product in the sale order line",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-promotion",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["sale_loyalty_order_suggestion", "sale_loyalty_criteria_multi_product"],
+    "installable": True,
+}
