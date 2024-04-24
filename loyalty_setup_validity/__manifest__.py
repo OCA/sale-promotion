a82843d13301596f6fe3b341729a554371690120
Damien <ultrarushgame@gmail.com>
2024-04-24 09:09:58 +0200

    [ADD] loyalty_setup_validity

---
+++ loyalty_setup_validity/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Moka Tourisme (https://www.mokatourisme.fr/).
+# @author Damien Horvat <ultrarushgame@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Loyalty Setup Validity",
+    "version": "16.0.1.0.0",
+    "category": "Sale",
+    "author": "Moka",
+    "website": "https://github.com/OCA/sale-promotion",
+    "summary": "Allow to define a fixed validity or a validity after the generation date of a loyalty card.",
+    "depends": ["loyalty"],
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
+    "data": ["views/loyalty_program_inherit.xml"],
+}
---
+++ loyalty_setup_validity/models/__init__.py
@@ -0,0 +1,2 @@
+from . import loyalty_card_inherit
+from . import loyalty_program_inherit
