874124a66d541e9eb9683977d16a183fcb711fc1
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2024-05-14 17:32:45 +0200

    [ADD] sale_promotion_discount_in_field_order_line_link: new module

---
+++ sale_promotion_discount_in_field_order_line_link/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Coupon Promotion Discount in Field Order Line Link",
+    "version": "14.0.1.0.0",
+    "summary": "Bridge module to track promotions on SOL " "with the discount in field",
+    "author": "PyTech SRL, Odoo Community Association (OCA)",
+    "maintainers": ["aleuffre", "renda-dev", "PicchiSeba"],
+    "license": "AGPL-3",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": [
+        "sale_coupon_order_line_link",
+        "sale_promotion_discount_in_field",
+    ],
+    "demo": [],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
