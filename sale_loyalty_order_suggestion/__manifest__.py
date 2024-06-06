fdef33ab010476755076a4d038f9f470b7d80404
Kev-Roche <kevin.roche@akretion.com>
2024-06-06 17:02:36 +0200

    [16.0][FIX] compute suggested_promotions when no product in so line

--- sale_loyalty_order_suggestion/__manifest__.py
+++ sale_loyalty_order_suggestion/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Loyalty Suggestions",
     "summary": "Suggest promotions in the sale order line",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-promotion",
