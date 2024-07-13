367ade2edc40968886f5b3e059b43ba7475bc3e3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-07-13 16:38:43 +0200

    [OU-FIX] loyalty_criteria_multi_product: column type + condition + criteria + repeat_product
    - It should be VARCHAR, not CHAR.
    - Remove the condition for assigning the rule (as there can be m2m
      linked products although there criteria selection is another).
    - Do the product m2m handle.
    - Do the repeat_product handlement by SQL + fix it.

--- loyalty_criteria_multi_product/migrations/16.0.1.0.0/post-migration.py
+++ loyalty_criteria_multi_product/migrations/16.0.1.0.0/post-migration.py
@@ -3,6 +3,22 @@
 from openupgradelib import openupgrade
 
 
+def _copy_multi_product_criteria(env):
+    """Copy multi-product criteria to the new m2m table. Done this way for avoiding to
+    rename columns, indexes, FKs, etc on the old m2m table, being this table not very
+    big for sure.
+    """
+    openupgrade.logged_query(
+        env.cr,
+        """
+        INSERT INTO loyalty_criteria_product_product_rel
+        (loyalty_criteria_id, product_product_id)
+        SELECT coupon_criteria_id, product_product_id
+        FROM coupon_criteria_product_product_rel
+        """,
+    )
+
+
 def adapt_rules_with_repeat_product(env):
     """Adapt rules with the 'Repeat' option to new promotion rules.
     The functionality of the 'Repeat' option has been integrated into the rules
@@ -11,17 +27,33 @@ def adapt_rules_with_repeat_product(env):
     For each loyalty criterion with 'Repeat' enabled, the corresponding loyalty rule
     is updated with the minimum quantity and product information. After the update,
     the loyalty criterion is unlinked."""
-    env.cr.execute("SELECT id FROM loyalty_criteria WHERE repeat_product = true")
-    results = env.cr.fetchall()
-    loyalty_criteria_ids = [result[0] for result in results]
-    for loyalty_criteria in env["loyalty.criteria"].browse(loyalty_criteria_ids):
-        rule = env["loyalty.rule"].browse(loyalty_criteria.rule_id)
-        rule.minimum_qty = loyalty_criteria.rule_min_quantity
-        rule.product_ids = rule.product_ids
-        loyalty_criteria.unlink()
-    env.cr.commit()
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE loyalty_rule lr
+        SET minimum_qty = lc.rule_min_quantity
+        FROM loyalty_criteria lc
+        WHERE lc.repeat_product
+            AND lc.rule_id = lr.id
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        INSERT INTO loyalty_rule_product_product_rel
+        (loyalty_rule_id, product_product_id)
+        SELECT lc.rule_id, rel2.product_product_id
+        FROM loyalty_criteria_product_product_rel rel2
+        JOIN loyalty_criteria lc ON lc.id = rel2.loyalty_criteria_id
+        WHERE lc.repeat_product
+        """,
+    )
+    openupgrade.logged_query(
+        env.cr, "DELETE FROM loyalty_criteria WHERE repeat_product"
+    )
 
 
 @openupgrade.migrate()
 def migrate(env, version):
+    _copy_multi_product_criteria(env)
     adapt_rules_with_repeat_product(env)
