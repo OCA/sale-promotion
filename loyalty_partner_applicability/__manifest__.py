cf362e0dbcd6557a466d1deb5a8797bcf3cd0e59
arantxa-s73 <arantxa@studio73.es>
2024-09-26 10:11:28 +0200

    [IMP] loyalty_partner_applicability: pre-commit auto fixes

--- loyalty_partner_applicability/__manifest__.py
+++ loyalty_partner_applicability/__manifest__.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Loyalty Partner Applicability",
-    "summary": "Enables the definition of a customer filter for promotion rules that will "
+    "summary": "Enables the definition of a customer filter for promotion "
+    "rules that will "
     "only be applied to customers who meet the specified conditions in the filter.",
     "version": "16.0.2.0.2",
     "category": "Sale",
