dfc14676c72c366fd8e14a1169b7219473e0fd6c
Bhavesh Heliconia <oca@heliconia.in>
2025-01-24 16:26:06 +0530

    [MIG] loyalty_multi_gift: Migration to 18.0

--- loyalty_multi_gift/tests/test_loyalty_multi_gift_case.py
+++ loyalty_multi_gift/tests/test_loyalty_multi_gift_case.py
@@ -1,10 +1,12 @@
 # Copyright 2021 Tecnativa - David Vidal
 # Copyright 2023 Tecnativa - Pilar Vargas
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import TransactionCase
+from odoo import Command
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class LoyaltyMultiGiftCase(TransactionCase):
+class LoyaltyMultiGiftCase(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -13,9 +15,7 @@ class LoyaltyMultiGiftCase(TransactionCase):
             {
                 "name": "Test pricelist",
                 "item_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
                             "applied_on": "3_global",
                             "compute_price": "formula",
@@ -47,9 +47,7 @@ class LoyaltyMultiGiftCase(TransactionCase):
                 "trigger": "auto",
                 "applies_on": "current",
                 "rule_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
                             "reward_point_mode": "order",
                             "minimum_qty": 2,
@@ -57,28 +55,24 @@ class LoyaltyMultiGiftCase(TransactionCase):
                     ),
                 ],
                 "reward_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
                             "reward_type": "multi_gift",
                             "required_points": 1,
                             "loyalty_multi_gift_ids": [
-                                (
-                                    0,
-                                    0,
+                                Command.create(
                                     {
-                                        "reward_product_ids": [(4, cls.product_2.id)],
+                                        "reward_product_ids": [
+                                            Command.link(cls.product_2.id)
+                                        ],
                                         "reward_product_quantity": 2,
                                     },
                                 ),
-                                (
-                                    0,
-                                    0,
+                                Command.create(
                                     {
                                         "reward_product_ids": [
-                                            (4, cls.product_3.id),
-                                            (4, cls.product_4.id),
+                                            Command.link(cls.product_3.id),
+                                            Command.link(cls.product_4.id),
                                         ],
                                         "reward_product_quantity": 3,
                                     },
@@ -103,11 +97,9 @@ class LoyaltyMultiGiftCase(TransactionCase):
         self.loyalty_program_form.reward_ids[0].write(
             {
                 "loyalty_multi_gift_ids": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
-                            "reward_product_ids": [(4, self.product_3.id)],
+                            "reward_product_ids": [Command.link(self.product_3.id)],
                             "reward_product_quantity": 1,
                         },
                     )
@@ -125,16 +117,15 @@ class LoyaltyMultiGiftCase(TransactionCase):
         self.loyalty_program_form.write(
             {
                 "reward_ids": [
-                    (
-                        1,
+                    Command.update(
                         self.loyalty_program_form.reward_ids[0].id,
                         {
                             "loyalty_multi_gift_ids": [
-                                (
-                                    0,
-                                    0,
+                                Command.create(
                                     {
-                                        "reward_product_ids": [(4, self.product_3.id)],
+                                        "reward_product_ids": [
+                                            Command.link(self.product_3.id)
+                                        ],
                                         "reward_product_quantity": 1,
                                     },
                                 )
@@ -148,3 +139,61 @@ class LoyaltyMultiGiftCase(TransactionCase):
             self.loyalty_program_form.reward_ids[0].loyalty_multi_gift_ids
         )
         self.assertEqual(gift_options_qty, 4)
+
+    def test_02_compute_multi_gift(self):
+        """Test the computation of multi_gift field."""
+        reward = self.loyalty_program_form.reward_ids[0]
+        reward.reward_type = "multi_gift"
+        reward.loyalty_multi_gift_ids = [Command.clear()]
+        self.assertFalse(
+            reward.multi_gift, "Multi gift should be False for empty gift list."
+        )
+        reward.loyalty_multi_gift_ids = [
+            Command.create(
+                {
+                    "reward_product_ids": [Command.link(self.product_2.id)],
+                    "reward_product_quantity": 1,
+                },
+            )
+        ]
+        self.assertTrue(
+            reward.multi_gift, "Multi gift should be True for valid gift list."
+        )
+
+    def test_03_compute_description(self):
+        """Test the description computation for multi_gift rewards."""
+        reward = self.loyalty_program_form.reward_ids[0]
+        reward.reward_type = "multi_gift"
+        reward.loyalty_multi_gift_ids = [Command.clear()]  # Clear existing lines
+        self.assertEqual(
+            reward.description,
+            "Multi Gift",
+            "Description should match for empty products.",
+        )
+        reward.loyalty_multi_gift_ids = [
+            Command.create(
+                {
+                    "reward_product_ids": [
+                        Command.link(self.product_2.id),
+                        Command.link(self.product_3.id),
+                    ],
+                },
+            )
+        ]
+        self.assertIn(
+            "Multi Gift -",
+            reward.description,
+            "Description should include product names.",
+        )
+
+    def test_04_onchange_reward_product_ids(self):
+        """Test onchange logic for reward_product_ids."""
+        line = self.loyalty_program_form.reward_ids[0].loyalty_multi_gift_ids[0]
+        line.reward_product_ids = [Command.set([self.product_3.id, self.product_2.id])]
+        line.onchange_reward_product_ids()
+        self.assertEqual(
+            line.reward_default_product_id.id,
+            self.product_3.id,
+            f"Expected {self.product_3.id} as default product, "
+            f"but got {line.reward_default_product_id.id}.",
+        )
