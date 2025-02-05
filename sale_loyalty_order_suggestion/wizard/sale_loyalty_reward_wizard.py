813c25abf777278e783c26596eeba3a4528d7187
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-02-05 08:35:35 +0100

    [UPG] repo template up to 1.29
    The 'actions/cachec1' into github workflow has been removed. This issue is fixed by the latest repo template

--- sale_loyalty_order_suggestion/wizard/sale_loyalty_reward_wizard.py
+++ sale_loyalty_order_suggestion/wizard/sale_loyalty_reward_wizard.py
@@ -85,7 +85,7 @@ class SaleLoyaltyRewardWizard(models.TransientModel):
                 products_str = f"{', '.join(products[:-1])} {_('or')} {products[-1]}"
             else:
                 products_str = products[0] if products else ""
-            self.loyalty_rule_line_description = f"<b>* {_('Required quantity')}:</b> {units_required} {_('units of')} {products_str}"  # noqa: B950
+            self.loyalty_rule_line_description = f"<b>* {_('Required quantity')}:</b> {units_required} {_('units of')} {products_str}"  # noqa: B950 E231
 
     def _update_order_line_with_units(self, order_line, units):
         """Updates an existing order line with the provided units."""
