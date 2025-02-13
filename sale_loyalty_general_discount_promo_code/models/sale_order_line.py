f2132e9a9ed126e69c1833391ac976518864637d
natuan9 <tuan3032001@gmail.com>
2025-02-13 12:11:22 +0700

    [ADD] sale_loyalty_general_discount_promo_code

---
+++ sale_loyalty_general_discount_promo_code/models/sale_order_line.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def apply_reward(self, reward):
+        self.ensure_one()
+        self.discount = reward.discount
+
+        # If Avatax Amount has already been computed, reset it to 0 so the user can recompute it
+        if getattr(self, "tax_amt", 0) > 0:
+            self.tax_amt = 0
+            self.order_id.tax_amount = 0
